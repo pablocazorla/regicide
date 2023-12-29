59e9253ca5fad9eed514635989607e8257ddfeb2
Pablo Cazorla <miscuentas2pablo@gmail.com>
2023-12-29 13:57:07 -0300

    Update index.jsx

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,6 +1,6 @@
 const Modal = ({ visible, children }) => {
   return visible ? (
-    <div className="fixed top-0 w-screen left-0 h-screen">
+    <div className="fixed top-0 w-screen left-0 h-screen bg-black/20 backdrop-blur-sm">
       <div className="container flex flex-col justify-center items-stretch p-4">
         <div className="bg-teal-700 overflow-auto rounded-lg shadow-xl">
           <div>{children}</div>
